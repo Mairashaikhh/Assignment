55d50891c2121722f2f2c5b247bcdf4925f486b3
Mairashaikhh <159727159+Mairashaikhh@users.noreply.github.com>
2024-02-27 00:54:54 +0500

    Exercise 12
    Typescript Assignment

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var members = ['rukhsar', 'ayesha', 'faiza', 'bisma', 'maira', 'usama', 'minhaj', 'umair'];
+var message = "Have you completed the assignment?";
 for (var i = 0; i < members.length; i++) {
-    console.log(members[i]);
+    console.log(message + members[i]);
 }
