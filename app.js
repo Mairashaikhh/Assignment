cf86322ae74a35bc9d66e9fddbe93196262ff964
Mairashaikhh <159727159+Mairashaikhh@users.noreply.github.com>
2024-02-27 00:51:10 +0500

    Exercise 10
    Typescript Assignment Exercise 10 program1

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
-var favoriteNumber = 5;
-console.log("My favorite number is ".concat(favoriteNumber));
+//Typescript Assignment Solution'10
+//My name is MAIRA
+//Date: 24/02/2024
+//This program will run simple code Hello World
+console.log("Hello World");
