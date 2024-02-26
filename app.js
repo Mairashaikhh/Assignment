1be116bcb9f144db787cdf1e3af96afa5128a3c0
Mairashaikhh <159727159+Mairashaikhh@users.noreply.github.com>
2024-02-27 01:00:35 +0500

    Exercise 16
    Typescript Assignment

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
-var guest_list = ['Rukhsar', 'Faiza', 'Bissma', 'Aizay'];
-for (var i = 0; i < guest_list.length; i++) {
-    console.log('Respected Madam ' + guest_list[i] + ',\nI invite you for dinner tonight.\nThank You\n');
-}
-//Changing Guest List
+//Guest List Exercise 14
+var guest_list = ['Rukhsar', 'Faiza', 'Bisma', 'Aizay'];
+// for(let i=0; i<guest_list.length; i++){
+//     console.log('Respected Madam ' + guest_list[i] + ',\nI invite you for dinner tonight.\nThank You\n')
+// }
+//Changing Guest List Exercise 15
 var not_present = 'Bisma';
 var new_guest = 'Ayesha';
 guest_list[2] = new_guest;
 for (var i = 0; i < guest_list.length; i++) {
     console.log('Respected Madam ' + guest_list[i] + ',\nI invite you for dinner tonight.\nThank You\n');
 }
-console.log("Madam ".concat(not_present, " is not coming tonight dinner. "));
+console.log("Madam ".concat(not_present, " is not coming tonight dinner.\n "));
+//More Guest Exercise 16
+guest_list.unshift('Numaila', 'Noor');
+for (var i = 0; i < guest_list.length; i++) {
+    console.log('Respected Madam ' + guest_list[i] + ',\nI invite you for dinner tonight. We got a big dinner table so we decided to invite two more guests.\nThank You\n');
+}
