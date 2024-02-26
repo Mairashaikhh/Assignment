88ef3ef2bae7cb0a046433983c517ed7e51d36ca
Mairashaikhh <159727159+Mairashaikhh@users.noreply.github.com>
2024-02-27 00:58:05 +0500

    Exercise 14 & 15
    Typescript Assignment

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-var transportation = ['Nissan', 'Hyundai Sonata', 'Mercedes', 'Audi', 'Lamborghini'];
-for (var i = 0; i < transportation.length; i++) {
-    console.log('I would like to own a ' + transportation[i]);
+var guest_list = ['Rukhsar', 'Faiza', 'Bissma', 'Aizay'];
+for (var i = 0; i < guest_list.length; i++) {
+    console.log('Respected Madam ' + guest_list[i] + ',\nI invite you for dinner tonight.\nThank You\n');
 }
+//Changing Guest List
+var not_present = 'Bisma';
+var new_guest = 'Ayesha';
+guest_list[2] = new_guest;
+for (var i = 0; i < guest_list.length; i++) {
+    console.log('Respected Madam ' + guest_list[i] + ',\nI invite you for dinner tonight.\nThank You\n');
+}
+console.log("Madam ".concat(not_present, " is not coming tonight dinner. "));
